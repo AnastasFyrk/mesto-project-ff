ae534fbeedc1af3e02f8906fe472799d4d64ea55
Anastasiya Semenchenko <Anastas.fyrk@yandex.ru>
2024-04-20 19:59:45 +0300

    Первая попытка 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,23 @@
 function createCard(name, link, deleteCard) {
-    const template = document.querySelector('#card-template');
-    const cardElement = template.content.cloneNode(true);
-    const deleteButton = cardElement.querySelector('.card__delete-button');
+  const template = document.querySelector("#card-template");
+  const cardElement = template.content.cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
-    cardElement.querySelector('.card__title').textContent = name;
-    cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector(".card__title").textContent = name;
+  cardElement.querySelector(".card__image").src = link;
 
-    deleteButton.addEventListener('click', (event) => deleteCard(event));
+  deleteButton.addEventListener("click", (event) => deleteCard(event));
 
-    return cardElement;
+  return cardElement;
 }
 
-function deleteCard () {
-    const card = event.target.closest('.card');
-    card.remove();
+function deleteCard() {
+  const card = event.target.closest(".card");
+  card.remove();
 }
 
-const cardList = document.querySelector('.places__list');
+const cardList = document.querySelector(".places__list");
 
 initialCards.forEach((item) => {
-    cardList.append(createCard(item.name, item.link, deleteCard));
-})
+  cardList.append(createCard(item.name, item.link, deleteCard));
+});
